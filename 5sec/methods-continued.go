1b18e341a0f9c7fcdc3743dbd64d12d02b5d28c1
yusuke ohno <y.ono.varial@gmail.com>
2018-11-14 16:09:43 +0900

    add 5sec

---
+++ 5sec/methods-continued.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type MyFloat float64
+
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func (f MyFloat) Inte() int{
+	return int(f)
+}
+
+func main() {
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+	fmt.Println(f.Inte())
+}
